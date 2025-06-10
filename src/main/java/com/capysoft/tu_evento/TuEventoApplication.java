21e9a14715b89557e8332b131a11e3de55af03c0
cristofer lozano <cristoferlozano233@gmail.com>
2025-06-10 08:58:39 -0500

    first commit

---
+++ src/main/java/com/capysoft/tu_evento/TuEventoApplication.java
@@ -0,0 +1,13 @@
+package com.capysoft.tu_evento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuEventoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capysoft/tu_evento/TuEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capysoft.tu_evento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
