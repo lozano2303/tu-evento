ffab3051f181d6ae4a88a0059daf8f6887698323
Cristofer <cristoferlozano233@gmail.com>
2025-08-07 11:33:04 -0500

    first-commit

---
+++ TuEventoBackend/src/main/java/TuEvento/Backend/TuEventoBackendApplication.java
@@ -0,0 +1,13 @@
+package TuEvento.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuEventoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuEventoBackendApplication.class, args);
+	}
+
+}
---
+++ TuEventoBackend/src/test/java/TuEvento/Backend/TuEventoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package TuEvento.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuEventoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
