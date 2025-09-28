def92ac5208be960cbe295d45f3ac6c7711af018
TheJhampierBoy <ortizjhampier@gmail.com>
2025-09-27 20:07:18 -0500

    fix: add missing AWS S3 configuration properties

--- TuEventoFrontend (Mobile)/App.tsx
+++ TuEventoFrontend (Mobile)/App.tsx
@@ -5,7 +5,7 @@ import * as WebBrowser from 'expo-web-browser';
 import 'react-native-gesture-handler';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-// Esto debe estar ANTES de cualquier componente
+
 WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
